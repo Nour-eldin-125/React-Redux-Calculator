dcfaa76669cba08cf5540c993076b59fc4d1091c
Noor Eldin El Sherbiny <noureldinelsherbiny5@gmail.com>
2023-08-17 18:30:10 +0300

    Adding Numbers function for the Numbers.js

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -1,16 +1,25 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+import { connect } from "react-redux";
+import { AddNumber } from "../store/actions";
+
 
 class Numbers extends React.Component {
     constructor(props) {
         super(props);
         console.log("Numbers",this.props.numbers)
     }
+    add (number){
+        console.log("Numbers Args:",this.props.firstArg, this.props.secondArg)
+        this.props.secondOpFlap ? this.props.addNumber(this.props.secondArg? ('' + this.props.secondArg + number): number) :this.props.addNumber(this.props.firstArg?(''+this.props.firstArg + number): number) 
+        
+    }
     render() {
         return (
             <>
                 {
                     this.props.numbers.map((num) => {
-                        return <button key={num}>{num}</button>
+                        return <button onClick= {() => this.add(num)} key={num}>{num}</button>
                     })
                 }
             </>
@@ -18,4 +27,20 @@ class Numbers extends React.Component {
     }
 }
 
-export default Numbers;
+const mapStateToProps = (state) =>{
+    return {
+        firstArg:state.firstArg,
+        secondArg:state.secondArg,
+        secondOpFlap:state.secondOpFlap,
+
+    }
+}
+
+const mapDispatchToProps = (dispatch) =>{
+    return {
+        addNumber: (payload) => dispatch(AddNumber(payload))
+    }
+}
+    
+
+export default connect (mapStateToProps, mapDispatchToProps)(Numbers);

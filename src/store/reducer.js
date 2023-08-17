86e99325b315bff8a33710606614a7cb9dce3546
Noor Eldin El Sherbiny <noureldinelsherbiny5@gmail.com>
2023-08-17 23:01:13 +0300

    adding function to reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,10 @@
 import { actionTypes } from "./actions";
 const initialState = {
-    firstArg:"",
+    firstArg:'',
     operator: "",
-    opFalg:false,
+    opFlag:false,
     secondArg:'',
-    secondOpFlap:false,
+    secondOpFlag:false,
     calculate:false,
     result:0
 
@@ -14,46 +14,82 @@ const {addNumber,addOperator,remove,calculate,clear} = actionTypes
 export default function reducer (state=initialState, action) {
     switch (action.type) {
         case addNumber:
-            console.log("Reducer : Action Payload:",action.payload)
-            if(state.secondOpFlap){
+            if (state.calculate){
+                return{
+                    ...initialState,firstArg:String(action.payload)
+                }
+            }
+            // console.log("Reducer : Action Payload:",action.payload)
+            if(state.secondOpFlag || state.opFlag){
                 console.log("Reducer If : Action Payload:",action.payload)
                 return{
-                    ...state,secondArg:action.payload
+                    ...state,secondArg:String(state.secondArg==0?"":state.secondArg)+String(action.payload), secondOpFlag:true , opFlag:false
                 }
             }
             else{
                 console.log("Reducer Else : Action Payload:",action.payload)
                 return{
-                    ...state,firstArg:action.payload
+                    ...state,firstArg:String(state.firstArg==0?"":state.firstArg)+String(action.payload)
                 }
             }
         case addOperator:
-            return {
-                ...state,operator : action.payload
+            if (state.calculate){
+                return{
+                    ...initialState,firstArg:state.result, opFlag:true , operator:action.payload
+                }
             }
+            else if (state.firstArg!=="") {
+                console.log("Reducer : AddOperator Action Payload:",action.payload)
+                return {
+                    ...state,operator:action.payload, opFlag:true
+                }
+            }
+            else {
+                console.log("Reducer : AddOperator Action Payload Error:",action.payload)
+                return {...state}
+            }
+            
         case clear:
             return {
-                ...state
+                ...initialState
             }
         case calculate:
             return {
-                ...state
-                
+                ...state,calculate:true,result:eval(state.secondArg+state.operator+state.firstArg)
             }
         case remove:
-            if(state.secondOpFlap){
-                return{
-                    ...state,secondArg:state.secondArg.slice(0, -1)
+            if (state.calculate) {
+                return {
+                    ...state,calculate:false, result:0, secondOpFlag:true,opFlag:false 
                 }
             }
-            else if (state.opFalg){
+            else if(state.secondOpFlag){
+                if(state.secondArg.length<=1){
+                    return{
+                        ...state,secondArg:"",opFlag:true , secondOpFlag:false
+                    }
+                }
+                else{
+                    return{
+                        ...state,secondArg:String(state.secondArg).slice(0, -1)
+                    }
+                }
+            }
+            else if (state.opFlag){
                 return{
-                    ...state,operator:"" , opFalg:false
+                    ...state,operator:"" , opFlag:false , secondOpFlag:false
                 }
             }
             else{
-                return{
-                    ...state,firstArg:state.firstArg.slice(0, -1)
+                if (state.firstArg.length <= 1) {
+                        return{
+                            ...state,firstArg:0
+                    }
+                }
+                else{                    
+                    return{
+                        ...state,firstArg:state.firstArg.length === 1 ? 0: String(state.firstArg).slice(0, -1)
+                    }
                 }
             }
         default:

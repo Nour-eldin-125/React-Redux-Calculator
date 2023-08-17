af3221f64e02f9a37b4d2fe3ddf26bb0378a494a
Noor Eldin El Sherbiny <noureldinelsherbiny5@gmail.com>
2023-08-17 23:00:05 +0300

    finishing operator function calls

--- src/components/Operators.js
+++ src/components/Operators.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Calculate, AddOperator, Clear, Remove } from "../store/actions";
 
 class Operators extends React.Component {
     constructor(props) {
@@ -8,7 +10,18 @@ class Operators extends React.Component {
             classname:''
         }
     }
-    
+    addOp(op) {
+        if(op === "="){
+            this.props.calculate()
+        }
+        else if (op === "C") {
+            this.props.remove()
+        }
+        else {
+            this.props.addOperator(op)
+        }
+    }
+
     componentDidMount() {
         this.setState({
             operator: this.props.operator.op,
@@ -29,6 +42,7 @@ class Operators extends React.Component {
         return (
             <>
                 <button className={this.state.classname}
+                    onClick={()=>this.addOp(this.state.operator)}
                     key={this.state.operator}>
                         {this.state.operator}
                 </button>  
@@ -36,6 +50,26 @@ class Operators extends React.Component {
         );
         
     }
+
+
 }
 
-export default Operators;
+const mapStateToProps = (state) => {
+    return {
+        firstArg: state.firstArg,
+        operator: state.operator,
+        secondArg: state.secondArg,
+        secondOpFlap: state.secondOpFlap,
+        calculate: state.calculate,
+        result: state.result
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addOperator: (payload) => dispatch(AddOperator(payload)),
+        calculate: () => dispatch(Calculate()),
+        clear: () => dispatch(Clear()),
+        remove: () => dispatch(Remove())
+    }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Operators);

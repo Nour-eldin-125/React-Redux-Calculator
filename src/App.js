d33ddb5c64154be307590ff560039e4bf18b7bb8
Noor Eldin El Sherbiny <noureldinelsherbiny5@gmail.com>
2023-08-17 15:07:35 +0300

    creating static design

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div class="calculator">
+  
+      <div class="Grid">
+          <input type="text" id="display" readonly = "true" />
+          <button onclick="appendToDisplay('7')">7</button>
+          <button onclick="appendToDisplay('8')">8</button>
+          <button onclick="appendToDisplay('9')">9</button>
+          <button class="operator" onclick="appendToDisplay('+')">+</button>
+          <button onclick="appendToDisplay('6')">6</button>
+          <button onclick="appendToDisplay('5')">5</button>
+          <button onclick="appendToDisplay('4')">4</button>
+          <button class="operator" onclick="appendToDisplay('-')">-</button>
+          <button onclick="appendToDisplay('3')">3</button>
+          <button onclick="appendToDisplay('2')">2</button>
+          <button onclick="appendToDisplay('1')">1</button>
+        <button class="operator" onclick="appendToDisplay('*')">*</button>
+        <button class="clear" onclick="clearDisplay()">C</button>
+        <button onclick="appendToDisplay('0')">0</button>
+        <button class="calculate" onclick="calculateResult()">=</button>
+        <button class="operator" onclick="appendToDisplay('/')">/</button>
+      </div>
     </div>
   );
 }

cf1804f9dc676e5dbe4fda86b2dd182dfd08c767
Noor Eldin El Sherbiny <noureldinelsherbiny5@gmail.com>
2023-08-17 18:29:11 +0300

    adding remove, add values to reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -14,8 +14,22 @@ const {addNumber,addOperator,remove,calculate,clear} = actionTypes
 export default function reducer (state=initialState, action) {
     switch (action.type) {
         case addNumber:
+            console.log("Reducer : Action Payload:",action.payload)
+            if(state.secondOpFlap){
+                console.log("Reducer If : Action Payload:",action.payload)
+                return{
+                    ...state,secondArg:action.payload
+                }
+            }
+            else{
+                console.log("Reducer Else : Action Payload:",action.payload)
+                return{
+                    ...state,firstArg:action.payload
+                }
+            }
+        case addOperator:
             return {
-                ...state
+                ...state,operator : action.payload
             }
         case clear:
             return {
@@ -27,8 +41,20 @@ export default function reducer (state=initialState, action) {
                 
             }
         case remove:
-            return {
-                ...state
+            if(state.secondOpFlap){
+                return{
+                    ...state,secondArg:state.secondArg.slice(0, -1)
+                }
+            }
+            else if (state.opFalg){
+                return{
+                    ...state,operator:"" , opFalg:false
+                }
+            }
+            else{
+                return{
+                    ...state,firstArg:state.firstArg.slice(0, -1)
+                }
             }
         default:
             return state
